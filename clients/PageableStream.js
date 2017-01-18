8db18d56fb11e188a6b1ba567673def80d8d2b20
Gregoire Weber <gregoire@MacBook-Pro-de-Gregoire.local>
2017-01-18 15:42:42 -0500

    Refactor and test PageableStream

--- clients/PageableStream.js
+++ clients/PageableStream.js
@@ -6,35 +6,42 @@ const PAGE_RECEIVED_EVENT = 'page';
 const LAST_PAGE_EVENT = 'end';
 const ERROR_EVENT = 'error';
 
+function getEventEmitter(object) {
+  return _eventEmitter.get(object);
+}
+
+function createEventEmitter(object) {
+  _eventEmitter.set(object, new EventEmitter());
+}
 
 class PageableStream {
 
   constructor() {
-    _eventEmitter.set(this, new EventEmitter());
+    createEventEmitter(this);
   }
 
   onPageReceived(cb) {
-    _eventEmitter.get(this).on(PAGE_RECEIVED_EVENT, cb);
+    getEventEmitter(this).on(PAGE_RECEIVED_EVENT, cb);
   }
 
   onError(cb) {
-    _eventEmitter.get(this).on(ERROR_EVENT, cb);
+    getEventEmitter(this).on(ERROR_EVENT, cb);
   }
 
   onLastPage(cb) {
-    _eventEmitter.get(this).on(LAST_PAGE_EVENT, cb);
+    getEventEmitter(this).on(LAST_PAGE_EVENT, cb);
   }
 
   write(page) {
-    _eventEmitter.get(this).emit(PAGE_RECEIVED_EVENT, page);
+    getEventEmitter(this).emit(PAGE_RECEIVED_EVENT, page);
   }
 
   fail(error) {
-    _eventEmitter.get(this).emit(ERROR_EVENT, error);
+    getEventEmitter(this).emit(ERROR_EVENT, error);
   }
 
   lastPage() {
-    _eventEmitter.get(this).emit(LAST_PAGE_EVENT);
+    getEventEmitter(this).emit(LAST_PAGE_EVENT);
   }
 
 }
--- clients/PageableStream.spec.js
+++ clients/PageableStream.spec.js
@@ -8,7 +8,6 @@ const PageableStream = require('./PageableStream');
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
-
 describe('PageableStream', () => {
 
   let pageableStream;
@@ -20,14 +19,53 @@ describe('PageableStream', () => {
       pageableStream = new PageableStream();
     });
   
-    it('should execute the callback when event received', () => {
+    it('should execute the callback when write event received', () => {
       // Given
-      const response = { data: 'Ok cool' };
-      const callback = sinon.stub().returns(Promise.resolve(response));
+      const page = { data: 'Ok cool' };
+      const callback = sinon.stub().returns(Promise.resolve());
 
       // When / Then
       pageableStream.onPageReceived(callback);
+      pageableStream.write(page);
       expect(callback).to.have.been.calledOnce;
+      expect(callback).to.have.been.calledWithExactly(page);
+    });
+  });
+
+  describe('#onError()', () => {
+
+    before(() => {
+      pageableStream = new PageableStream();
+    });
+  
+    it('should execute the callback when error event received', () => {
+      // Given
+      const error = { data: 'Ok pas cool' };
+      const callback = sinon.stub().returns(Promise.resolve());
+
+      // When / Then
+      pageableStream.onError(callback);
+      pageableStream.fail(error);
+      expect(callback).to.have.been.calledOnce;
+      expect(callback).to.have.been.calledWithExactly(error);
+    });
+  });
+
+  describe('#onLastPage()', () => {
+
+    before(() => {
+      pageableStream = new PageableStream();
+    });
+  
+    it('should execute the callback when lastPage event received', () => {
+      // Given
+      const callback = sinon.stub().returns(Promise.resolve());
+
+      // When / Then
+      pageableStream.onLastPage(callback);
+      pageableStream.lastPage();
+      expect(callback).to.have.been.calledOnce;
+      expect(callback).to.have.been.calledWithExactly();
     });
   });
 });
