41f449648962ca62884996fc95fb7b9fafc28c97
Gregoire Weber <gregoire@barracks.io>
2017-02-06 17:15:51 -0500

    Add option to download update if one avalaible on check update

--- commands/CheckUpdateCommand.js
+++ commands/CheckUpdateCommand.js
@@ -4,18 +4,26 @@ class CheckUpdateCommand extends BarracksCommand {
 
   configureCommand(program) {
     return program
-      .arguments('<emulated-device>');
+      .arguments('<emulated-device>')
+      .option('--download [path]', 'If an update is available, download the package associated to the specified folder');
   }
 
   validateCommand(program) {
-    return !!program.args[0];
+    const validArg = !!program.args[0];
+    const validOptions = !program.download || (program.download && program.download !== true);
+    return validArg && validOptions;
   }
 
   execute(program) {
     return this.getAuthenticationToken().then(token => {
       return this.barracks.getAccount(token);
     }).then(account => {
-      return this.barracks.checkUpdate(account.apiKey, JSON.parse(program.args[0]));
+      const device = JSON.parse(program.args[0]);
+      if (program.download) {
+        return this.barracks.checkUpdateAndDownload(account.apiKey, device, program.download);
+      } else {
+        return this.barracks.checkUpdate(account.apiKey, device);
+      }
     });
   }
 
--- commands/CheckUpdateCommand.spec.js
+++ commands/CheckUpdateCommand.spec.js
@@ -15,11 +15,10 @@ describe('CheckUpdateCommand', () => {
   const unitId = 'testCLI_1';
   const versionId = '0.1';
   const apiKey = '1234567890poiuytrewq';
+  const downloadPath = 'path/to/download/';
   const programWithValidArgs = {
-    args: [
-      '{ "unitId":"' + unitId + '", "versionId":"' + versionId + '"}',
-      apiKey
-    ]
+    args: ['{ "unitId":"' + unitId + '", "versionId":"' + versionId + '"}'],
+    download: downloadPath
   };
 
   describe('#validateCommand(program)', () => {
@@ -30,24 +29,47 @@ describe('CheckUpdateCommand', () => {
       checkUpdateCommand.userConfiguration = {};
     });
   
-    it('should return true when device given', () => {
+    it('should return true when device and download path given', () => {
       // Given
-      const program = { args: ['{ "unitId":"testCLI_1", "versionId":"0.1"}'] };
-
+      const program = programWithValidArgs;
       // When
       const result = checkUpdateCommand.validateCommand(program);
-
       // Then
       expect(result).to.be.true;
     });
 
-    it('should return false when no param given', () => {
+    it('should return true when device and no option given', () => {
       // Given
-      const program = { args: [] };
-
+      const program = {args: ['{ "unitId":"' + unitId + '", "versionId":"' + versionId + '"}']};
       // When
       const result = checkUpdateCommand.validateCommand(program);
+      // Then
+      expect(result).to.be.true;
+    });
 
+    it('should return false when device given and download option without path', () => {
+      // Given
+      const program = Object.assign({}, programWithValidArgs, { download: true });
+      // When
+      const result = checkUpdateCommand.validateCommand(program);
+      // Then
+      expect(result).to.be.false;
+    });
+
+    it('should return false when no device given with download option', () => {
+      // Given
+      const program = Object.assign({}, programWithValidArgs, { args: [] });
+      // When
+      const result = checkUpdateCommand.validateCommand(program);
+      // Then
+      expect(result).to.be.false;
+    });
+
+    it('should return false when no device given with no option', () => {
+      // Given
+      const program = { args: [] };
+      // When
+      const result = checkUpdateCommand.validateCommand(program);
       // Then
       expect(result).to.be.false;
     });
@@ -63,7 +85,7 @@ describe('CheckUpdateCommand', () => {
   
     it('should call client to check update', done => {
       // Given
-      const program = programWithValidArgs;
+      const program = Object.assign({}, programWithValidArgs, { download: undefined });
       const account = { apiKey };
       const response = 'cool';
       checkUpdateCommand.getAuthenticationToken = sinon.stub().returns(Promise.resolve(token));
@@ -83,6 +105,36 @@ describe('CheckUpdateCommand', () => {
         expect(checkUpdateCommand.barracks.checkUpdate).to.have.been.calledWithExactly(
           apiKey,
           { unitId, versionId }
+        );
+        done();
+      }).catch(err => {
+        done(err);
+      });
+    });
+
+    it('should call client to check and download update when download option given', done => {
+      // Given
+      const program = programWithValidArgs;
+      const account = { apiKey };
+      const response = 'cool';
+      checkUpdateCommand.getAuthenticationToken = sinon.stub().returns(Promise.resolve(token));
+      checkUpdateCommand.barracks = {
+        getAccount: sinon.stub().returns(Promise.resolve(account)),
+        checkUpdateAndDownload: sinon.stub().returns(Promise.resolve(response))
+      };
+
+      // When / Then
+      checkUpdateCommand.execute(program).then(result => {
+        expect(result).to.equal(response);
+        expect(checkUpdateCommand.getAuthenticationToken).to.have.been.calledOnce;
+        expect(checkUpdateCommand.getAuthenticationToken).to.have.been.calledWithExactly();
+        expect(checkUpdateCommand.barracks.getAccount).to.have.been.calledOnce;
+        expect(checkUpdateCommand.barracks.getAccount).to.have.been.calledWithExactly(token);
+        expect(checkUpdateCommand.barracks.checkUpdateAndDownload).to.have.been.calledOnce;
+        expect(checkUpdateCommand.barracks.checkUpdateAndDownload).to.have.been.calledWithExactly(
+          apiKey,
+          { unitId, versionId },
+          downloadPath
         );
         done();
       }).catch(err => {
