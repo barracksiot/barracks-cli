ebab5bc86233287b6f1c5956ebf3536806fafd0d
Gregoire Weber <gregoire@barracks.io>
2017-02-15 11:28:45 -0500

    Add support for filter option in device command

--- commands/DevicesCommand.js
+++ commands/DevicesCommand.js
@@ -51,17 +51,46 @@ function getAllDevicesFromSegment(token, barracks, segmentName) {
   });
 }
 
+function getFilteredDevices(token, barracks, filterName) {
+  return new Promise((resolve, reject) => {
+    barracks.getFilterByName(token, filterName).then(filter => {
+      return barracks.getFilteredDevices(token, filter.query);
+    }).then(resultStream => {
+      resolve(resultStream);
+    }).catch(err => {
+      reject(err);
+    });
+  });
+}
+
+function checkExclusiveOptions(program) {
+  const validSegment = !!(program.segment && program.segment !== true);
+  const validFilter = !!(program.filter && program.filter !== true);
+  return validFilter ? !validSegment : validSegment;
+}
+
 class DevicesCommand extends BarracksCommand {
+
+  validateCommand(program) {
+    if (program.segment || program.filter) {
+      return checkExclusiveOptions(program);
+    } else {
+      return true;
+    }
+  }
 
   configureCommand(program) {
     return program
-      .option('--segment [segmentName]', '(Optionnal) Render devices from that segment only');
+      .option('--segment [segmentName]', '(Optionnal) Render devices from that segment only (Cannot be used with --filter).')
+      .option('--filter [filterName]', '(Optionnal) Apply a filter on the device list (Cannot be used with --segment).');
   }
 
   execute(program) {
     return this.getAuthenticationToken().then(token => {
       if (program.segment) {
         return getAllDevicesFromSegment(token, this.barracks, program.segment);
+      } else if (program.filter) {
+        return getFilteredDevices(token, this.barracks, program.filter);
       } else {
         return getAllDevices(token, this.barracks);
       }
--- commands/DevicesCommand.spec.js
+++ commands/DevicesCommand.spec.js
@@ -13,7 +13,9 @@ describe('DevicesCommand', () => {
 
   let devicesCommand;
   const token = 'i8uhkj.token.65ryft';
-  const programWithValidOptions = {};
+  const programWithNoOption = {};
+  const programWithValidSegment = { segment: 'coucou' };
+  const programWithValidFilter = { filter: 'recoucou' };
 
   before(() => {
     devicesCommand = new DevicesCommand();
@@ -23,23 +25,121 @@ describe('DevicesCommand', () => {
 
   describe('#validateCommand(program)', () => {
 
-    it('should return true', () => {
+    it('should return true when no option given', () => {
       // Given
-      const program = {};
-
+      const program = programWithNoOption;
       // When
       const result = devicesCommand.validateCommand(program);
-
       // Then
       expect(result).to.be.true;
+    });
+
+    it('should return true when only segment option given', () => {
+      // Given
+      const program = programWithValidSegment;
+      // When
+      const result = devicesCommand.validateCommand(program);
+      // Then
+      expect(result).to.be.true;
+    });
+
+    it('should return true when only filter option given', () => {
+      // Given
+      const program = programWithValidFilter;
+      // When
+      const result = devicesCommand.validateCommand(program);
+      // Then
+      expect(result).to.be.true;
+    });
+
+    it('should return false when both filter and segment options given', () => {
+      // Given
+      const program = Object.assign({}, programWithValidFilter, programWithValidSegment);
+      // When
+      const result = devicesCommand.validateCommand(program);
+      // Then
+      expect(result).to.be.false;
     });
   });
 
   describe('#execute(program)', () => {
 
+    const filterName = programWithValidFilter.filter;
+    const query = { eq: { unitId: 'plop' } };
+    const filter = {
+      name: filterName,
+      query
+    };
+
+    it('should reject an error when a filter is given and no filter found', done => {
+      // Given
+      const program = programWithValidFilter;
+      const error = 'A pas trouvé le filter';
+      devicesCommand.getAuthenticationToken = sinon.stub().returns(Promise.resolve(token));
+      devicesCommand.barracks.getFilterByName = sinon.stub().returns(Promise.reject(error));
+
+      // when / Then
+      devicesCommand.execute(program).then(result => {
+        done('should have failed');
+      }).catch(err => {
+        expect(err).to.be.equals(error);
+        expect(devicesCommand.getAuthenticationToken).to.have.been.calledOnce;
+        expect(devicesCommand.getAuthenticationToken).to.have.been.calledWithExactly();
+        expect(devicesCommand.barracks.getFilterByName).to.have.been.calledOnce;
+        expect(devicesCommand.barracks.getFilterByName).to.have.been.calledWithExactly(token, filterName);
+        done();
+      });
+    });
+
+    it('should reject an error when a filter is given and get devices request fail', done => {
+      // Given
+      const program = programWithValidFilter;
+      const error = 'request failed';
+      devicesCommand.getAuthenticationToken = sinon.stub().returns(Promise.resolve(token));
+      devicesCommand.barracks.getFilterByName = sinon.stub().returns(Promise.resolve(filter));
+      devicesCommand.barracks.getFilteredDevices = sinon.stub().returns(Promise.reject(error));
+
+      // when / Then
+      devicesCommand.execute(program).then(result => {
+        done('should have failed');
+      }).catch(err => {
+        expect(err).to.be.equals(error);
+        expect(devicesCommand.getAuthenticationToken).to.have.been.calledOnce;
+        expect(devicesCommand.getAuthenticationToken).to.have.been.calledWithExactly();
+        expect(devicesCommand.barracks.getFilterByName).to.have.been.calledOnce;
+        expect(devicesCommand.barracks.getFilterByName).to.have.been.calledWithExactly(token, filterName);
+        expect(devicesCommand.barracks.getFilteredDevices).to.have.been.calledOnce;
+        expect(devicesCommand.barracks.getFilteredDevices).to.have.been.calledWithExactly(token, query);
+        done();
+      });
+    });
+
+    it('should return a PageableStream object when a filter is given', done => {
+      // Given
+      const program = programWithValidFilter;
+      devicesCommand.getAuthenticationToken = sinon.stub().returns(Promise.resolve(token));
+      devicesCommand.barracks.getFilterByName = sinon.stub().returns(Promise.resolve(filter));
+      devicesCommand.barracks.getFilteredDevices = sinon.stub().returns(Promise.resolve(new PageableStream()));
+
+      // when / Then
+      devicesCommand.execute(program).then(result => {
+        expect(result).to.be.instanceOf(PageableStream);
+        expect(devicesCommand.getAuthenticationToken).to.have.been.calledOnce;
+        expect(devicesCommand.getAuthenticationToken).to.have.been.calledWithExactly();
+        expect(devicesCommand.barracks.getFilterByName).to.have.been.calledOnce;
+        expect(devicesCommand.barracks.getFilterByName).to.have.been.calledWithExactly(token, filterName);
+        expect(devicesCommand.barracks.getFilteredDevices).to.have.been.calledOnce;
+        expect(devicesCommand.barracks.getFilteredDevices).to.have.been.calledWithExactly(token, query);
+        done();
+      }).catch(err => {
+        done(err);
+      });
+    });
+
+/*
     it('should return a PageableStream object when services return data', done => {
       // Given
-      const program = Object.assign({}, programWithValidOptions);
+      const program = Object.assign({}, programWithNoOption);
       const channels = [{ name: 'channel1' }, { name: 'channel2' }];
       devicesCommand.getAuthenticationToken = sinon.stub().returns(Promise.resolve(token));
       devicesCommand.barracks = {
@@ -49,17 +149,16 @@ describe('DevicesCommand', () => {
 
       // When / Then
       devicesCommand.execute(program).then(result => {
-        /*
         expect(result).to.be.equals(errorMessage);
         expect(devicesCommand.getAuthenticationToken).to.have.been.calledOnce;
         expect(devicesCommand.getAuthenticationToken).to.have.been.calledWithExactly();
         expect(devicesCommand.barracks.getDeviceEvents).to.have.been.calledOnce;
         expect(devicesCommand.barracks.getDeviceEvents).to.have.been.calledWithExactly(token, program.unitId);
-        */
         done();
       }).catch(err => {
         done(err);        
       });
     });
+  */
   });
 });
