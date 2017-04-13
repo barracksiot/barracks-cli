e5ece6d37fcbc63652e07c702068493ba4b2eae8
Grégoire Weber <gregoire@barracks.io>
2017-04-13 16:22:14 -0400

    Update get package command to match new argument format

--- src/commands/package/PackageCommand.js
+++ src/commands/package/PackageCommand.js
@@ -3,20 +3,16 @@ const BarracksCommand = require('../BarracksCommand');
 class PackageCommand extends BarracksCommand {
 
   validateCommand(program) {
-    return !!(
-        program.packageReference &&
-            program.packageReference !== true
-    );
+    return !!program.args[0];
   }
 
   configureCommand(program) {
-    return program
-        .option('--packageReference [value]', 'The reference of the package');
+    return program.arguments('<package-reference>');
   }
 
   execute(program) {
     return this.getAuthenticationToken().then(token => {
-      return this.barracks.getPackage(token, program.packageReference);
+      return this.barracks.getPackage(token, program.args[0]);
     });
   }
 }
