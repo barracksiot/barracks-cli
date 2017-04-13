386f9510f8059a27ab0c220915316d94f633c0aa
Grégoire Weber <gregoire@barracks.io>
2017-04-13 19:05:50 -0400

    Changed arguments of get version command

--- src/commands/package/PackageVersionsCommand.js
+++ src/commands/package/PackageVersionsCommand.js
@@ -3,20 +3,17 @@ const BarracksCommand = require('../BarracksCommand');
 class PackageVersionsCommand extends BarracksCommand {
 
   configureCommand(program) {
-    return program
-      .option('--packageReference [value]', 'The reference of the package to get the versions from');
+      .arguments('<package-reference>')
+      .arguments('<version-id>');
   }
 
   validateCommand(program) {
-    return !!(
-      program.packageReference &&
-      program.packageReference !== true
-    );
+    return !!(program.args && program.args.length === 2);
   }
 
   execute(program) {
     return this.getAuthenticationToken().then(token => {
-      return this.barracks.getComponentVersions(token, program.packageReference);
+      return this.barracks.getComponentVersion(token, program.args[0], program.args[1]);
     });
   }
 }
