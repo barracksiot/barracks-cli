ee26ac5b99b3d3b7e1ce4baed20c3222fc94905a
Grégoire Weber <gregoire@barracks.io>
2017-04-18 16:01:21 -0400

    Update LoginCommand to generate an api token and use it to connect to barracks

--- src/commands/LoginCommand.js
+++ src/commands/LoginCommand.js
@@ -1,4 +1,7 @@
 const BarracksCommand = require('./BarracksCommand');
+const os = require('os');
+
+const TOKEN_LABEL_MAX_LENGTH = 50;
 
 function loginOptionsGiven(program) {
   return program.email && program.email !== true && 
@@ -15,6 +18,13 @@ function getCredentials(loginCommand, program) {
   return loginCommand.requestUserAuthentication();
 }
 
+function buildTokenLabel() {
+  // max 50 char;
+  const hostname = os.hostname();
+  const user = os.userInfo().username;
+  return `${user}@${hostname}`.substring(0, TOKEN_LABEL_MAX_LENGTH);
+}
+
 class LoginCommand extends BarracksCommand {
 
   configureCommand(program) {
@@ -26,6 +36,10 @@ class LoginCommand extends BarracksCommand {
   execute(program) {
     return getCredentials(this, program).then(credentials => {
       return this.authenticate(credentials.email, credentials.password);
+    }).then(authToken => {
+      return this.barracks.createToken(authToken, { label: buildTokenLabel() });
+    }).then(apiToken => {
+      return this.saveAuthenticationToken(apiToken.value);
     }).then(() => {
       return Promise.resolve('Authentication successful');
     });
