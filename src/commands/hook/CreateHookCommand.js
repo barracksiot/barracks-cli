81022f2c7d29ad735c7c221f88383aa00fec32ff
Rémi <remi@barracks.io>
2017-08-11 16:22:17 -0400

    Refactor create hook command

--- src/commands/hook/CreateHookCommand.js
+++ src/commands/hook/CreateHookCommand.js
@@ -3,26 +3,26 @@ const Validator = require('../../utils/Validator');
 const ObjectReader = require('../../utils/ObjectReader');
 
 function getHookType(program) {
-  if (program.web) {
+  if (program.hookType === 'web') {
     return 'web';
-  } else if (program.googleAnalytics) {
+  } else if (program.hookType === 'googleAnalytics') {
     return 'google_analytics';
-  } else if (program.bigquery) {
+  } else if (program.hookType === 'bigQuery') {
     return 'bigquery';
   }
 }
 
 function getEventType(program) {
-  if (program.ping) {
+  if (program.event === 'ping') {
     return 'PING';
   }
-  if (program.enrollment) {
+  if (program.event === 'enrollment') {
     return 'ENROLLMENT';
   }
-  if(program.deviceDataChange) {
+  if(program.event === 'deviceDataChange') {
     return 'DEVICE_DATA_CHANGE';
   }
-  if(program.devicePackageChange) {
+  if(program.event === 'devicePackageChange') {
     return 'DEVICE_PACKAGE_CHANGE';
   }
 }
@@ -32,29 +32,27 @@ function getGoogleClientSecret(program) {
 }
 
 function hasValidEventType(program) {
-  return (!program.ping && program.enrollment && !program.deviceDataChange && !program.devicePackageChange ||
-        program.ping && !program.enrollment && !program.deviceDataChange && !program.devicePackageChange  ||
-        !program.ping && !program.enrollment && program.deviceDataChange && !program.devicePackageChange  ||
-        !program.ping && !program.enrollment && !program.deviceDataChange && program.devicePackageChange);
+  return (program.event && program.event !== true && 
+    ['ping', 'enrollment', 'deviceDataChange', 'devicePackageChange'].indexOf(program.event) > -1);
 }
 
 function hasValidHookTypeAndArguments(program) {
-  return (program.web && !program.googleAnalytics && !program.bigquery && program.url && program.url !== true ||
-        !program.web && program.googleAnalytics && !program.bigquery && program.gaTrackingId && program.gaTrackingId !== true ||
-        !program.web && !program.googleAnalytics && program.bigquery && program.googleClientSecret && Validator.fileExists(program.googleClientSecret));
+
+  var handlers = { 
+    web: (program) => program.url && program.url !== true,
+    googleAnalytics: (program) => program.gaTrackingId && program.gaTrackingId !== true,
+    bigQuery: (program) => program.googleClientSecret && program.googleClientSecret !== true && Validator.fileExists(program.googleClientSecret)
+  } 
+
+  return (['web', 'googleAnalytics', 'bigQuery'].indexOf(program.hookType) > -1) && handlers[program.hookType](program)
 }
 
 class CreateHookCommand extends BarracksCommand {
 
   configureCommand(program) {
     return program
-      .option('--ping', 'To create a hook triggered by the ping of a device.')
-      .option('--enrollment', 'To create a hook for the first ping of a device.')
-      .option('--deviceDataChange', 'To create a hook triggered when a device pings with new custom client data.')
-      .option('--devicePackageChange', 'To create a hook triggered when a device pings with new packages.')
-      .option('--web', 'To create a web hook')
-      .option('--googleAnalytics', 'To create a Google Analytics hook')
-      .option('--bigquery', 'To create a BigQuery hook')
+      .option('--event', 'To specify the type of event that triggers the hook (ping, enrollment, deviceDataChange or devicePackageChange).')
+      .option('--hookType', 'To specify the type of hook we want to create (web, googleAnalytics or bigQuery)')
       .option('--name [value]', 'The unique name of the webhook')
       .option('--gaTrackingId [value]', 'The trackingId for the Google Analytics account')
       .option('--googleClientSecret [path/to/file]', 'The path to the file with the Google client secret json used to authenticate to BigQuery.')
